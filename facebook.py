66ec2877754ca9360087294fb2ccb76f236295b8
hansglz <glzhang23@gmail.com>
2022-10-07 16:16:10 -0400

    add fb case test

---
+++ facebook.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+from random import randint
+
+facebook = pd.read_csv(
+    "data/facebook_combined.txt.gz",
+    compression="gzip",
+    sep=" ",
+    names=["start_node", "end_node"],
+)
+
+G = nx.from_pandas_edgelist(facebook, "start_node", "end_node")
+
+print("Number of nodes inside G: " + str(G.number_of_nodes()))
+print("Number of edges inside G: " + str(G.number_of_edges()))
+
+fig, ax = plt.subplots(figsize=(15, 9))
+ax.axis("off")
+plot_options = {"node_size": 10, "with_labels": False, "width": 0.15}
+nx.draw_networkx(G, pos=nx.random_layout(G), ax=ax, **plot_options)
+

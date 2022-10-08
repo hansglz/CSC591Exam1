c892d0b997eeb1564cdcee7f3d46bfd5789651f0
hansglz <glzhang23@gmail.com>
2022-10-08 15:04:53 -0400

    update problem 1 question a

--- facebook.py
+++ facebook.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 from random import randint
 
 facebook = pd.read_csv(
-    "data/facebook_combined.txt.gz",
+    "data/facebook/facebook_combined.txt.gz",
     compression="gzip",
     sep=" ",
     names=["start_node", "end_node"],
@@ -15,6 +15,7 @@ G = nx.from_pandas_edgelist(facebook, "start_node", "end_node")
 
 print("Number of nodes inside G: " + str(G.number_of_nodes()))
 print("Number of edges inside G: " + str(G.number_of_edges()))
+print("Is G connected? " + str(nx.is_connected(G)))
 
 fig, ax = plt.subplots(figsize=(15, 9))
 ax.axis("off")
--- problem1.py
+++ problem1.py
@@ -11,44 +11,66 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 facebook = pd.read_csv(
-    "data/facebook_combined.txt.gz",
+    "data/facebook/facebook_combined.txt.gz",
     compression="gzip",
     sep=" ",
     names=["start_node", "end_node"],
 )
 
-""" Load Graph """
-G = nx.from_pandas_edgelist(facebook, "start_node", "end_node")
-# M = nx.to_scipy_sparse_matrix(G)
-degree_sequence = sorted([d for n, d in G.degree()], reverse=True)
-degreeCount = collections.Counter(degree_sequence)
-deg, cnt = zip(*degreeCount.items())
-cs = np.cumsum(cnt)
+twitch = pd.read_csv(
+    "data/twitch/musae_ENGB_edges.csv",
+    sep=",",
+    names=["start_node", "end_node"]
+)
 
-# plt.loglog(deg, cs, 'bo')
-plt.plot(deg, cs, 'bo')
-plt.xscale('log')
-plt.yscale('log')
-plt.title("Cumulative Distribution plot log-log")
-plt.ylabel("Sample with value > Degree")
-plt.xlabel("Degree")
-plt.show()
-plt.clf()
+github = pd.read_csv(
+    "data/github/musae_git_edges.csv",
+    sep=",",
+    names=["start_node", "end_node"]
+)
 
-# plt.semilogy(deg, cs, 'bo')
-plt.plot(deg, cs, 'bo')
-plt.xscale('log')
-plt.title("Cumulative Distribution plot semi-log")
-plt.ylabel("Sample with value > Degree")
-plt.xlabel("Degree")
-plt.show()
-plt.clf()
+data = {'facebook': facebook, 'twitch': twitch, 'github': github}
 
-plt.plot(deg, cs, 'bo')
-plt.xscale('linear')
-plt.yscale('linear')
-plt.title("Cumulative Distribution plot linear-linear")
-plt.ylabel("Sample with value > Degree")
-plt.xlabel("Degree")
-plt.show()
-plt.clf()
+def load_graph(data):
+    g = nx.from_pandas_edgelist(data, "start_node", "end_node")
+    degree_sequence = sorted([d for n, d in g.degree()], reverse=True)
+    degreeCount = collections.Counter(degree_sequence)
+    deg, cnt = zip(*degreeCount.items())
+    cs = np.cumsum(cnt)
+    return deg, cs
+
+def question_a():
+    for key in data:
+        deg, cs = load_graph(data[key])
+        # plt.loglog(deg, cs, 'bo')
+        plt.plot(deg, cs, 'bo')
+        plt.xscale('log')
+        plt.yscale('log')
+        plt.title("Cumulative Distribution plot log-log")
+        plt.ylabel("Sample with value > Degree")
+        plt.xlabel("Degree")
+        # plt.show()
+        plt.savefig("image/" + key + "_log-log")
+        plt.clf()
+
+        # plt.semilogy(deg, cs, 'bo')
+        plt.plot(deg, cs, 'bo')
+        plt.xscale('log')
+        plt.title("Cumulative Distribution plot semi-log")
+        plt.ylabel("Sample with value > Degree")
+        plt.xlabel("Degree")
+        # plt.show()
+        plt.savefig("image/" + key + "_semi-log")
+        plt.clf()
+
+        plt.plot(deg, cs, 'bo')
+        plt.xscale('linear')
+        plt.yscale('linear')
+        plt.title("Cumulative Distribution plot linear-linear")
+        plt.ylabel("Sample with value > Degree")
+        plt.xlabel("Degree")
+        # plt.show()
+        plt.savefig("image/" + key + "_linear-linear")
+        plt.clf()
+
+question_a()
